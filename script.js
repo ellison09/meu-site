16d46ea981e662998830c714236b8d69a7844a9d
Ellison <134451845+ellison09@users.noreply.github.com>
2023-05-23 20:24:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,56 @@
-document.getElementById('loginForm').addEventListener('submit', function (event) {
+// Função para tratar o evento de cadastro
+async function cadastrar(event) {
     event.preventDefault();
 
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
+    const nome = document.getElementById('nomeCadastro').value;
+    const email = document.getElementById('emailCadastro').value;
+    const senha = document.getElementById('senhaCadastro').value;
 
-    if (email && password) {
-        // Aqui você pode adicionar a lógica de autenticação
+    const response = await fetch('URL_DA_API/cadastrar', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ nome, email, senha })
+    });
 
-        // Exemplo de mensagem de sucesso
-        showMessage('success', 'Login realizado com sucesso!');
+    if (response.ok) {
+        const message = document.getElementById('message');
+        message.innerHTML = 'Cadastro realizado com sucesso!';
+        message.style.color = 'green';
     } else {
-        // Exemplo de mensagem de erro
-        showMessage('error', 'Por favor, preencha todos os campos.');
+        const errorMessage = await response.text();
+        console.log(errorMessage); // Exibir o erro no console ou tratar de acordo com a necessidade
     }
-});
-
-function showMessage(type, message) {
-    var messageDiv = document.getElementById('message');
-    messageDiv.innerHTML = message;
-    messageDiv.className = type;
 }
+
+// Função para tratar o evento de login
+async function login(event) {
+    event.preventDefault();
+
+    const email = document.getElementById('emailLogin').value;
+    const senha = document.getElementById('senhaLogin').value;
+
+    const response = await fetch('URL_DA_API/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email, senha })
+    });
+
+    if (response.ok) {
+        const message = document.getElementById('message');
+        message.innerHTML = 'Login realizado com sucesso!';
+        message.style.color = 'green';
+    } else {
+        const errorMessage = await response.text();
+        console.log(errorMessage); // Exibir o erro no console ou tratar de acordo com a necessidade
+    }
+}
+
+const registerForm = document.getElementById('registerForm');
+registerForm.addEventListener('submit', cadastrar);
+
+const loginForm = document.getElementById('loginForm');
+loginForm.addEventListener('submit', login);
